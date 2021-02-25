847c2f11364f30ca1e85ec665a07cb29b49b3aac
repl.it user <replituser@example.com>
2021-02-25 04:30:43 +0000

    .

--- basics/output/escape_characters.py
+++
@@ -1,6 +0,0 @@
-# display escape characters
-print("\n diplay a new line")
-print("\t display a tab space")
-print("\\ display a back slash")
-print("\" display a double quote")
-print("\'display a single quote")
