b7d6569c190b35d7f2a1ecdcff95adc7be6955ae
repl.it user <replituser@example.com>
2021-02-27 16:09:38 +0000

    added code to demonstrate the use of a while loop in performine use of a while loop in performing calculations with user input.

---
+++ basics/repetitions/while_loop/sum_user_numbers.py
@@ -0,0 +1,20 @@
+# ask user for how many numbers to add up
+print("How many numbers should I sum up?")
+number_to_sum = int(input())
+
+# declare a control variable
+summed = 1
+
+# diplay blank line
+
+# sum numbers
+total = 0
+
+while (summed <= number_to_sum):
+  print("Please enter number", summed, "of", number_to_sum, ":" )
+  number = int(input())
+  total = total + number
+  summed = summed +1
+
+# display result
+print("\nThe answer is {}".format(total))
