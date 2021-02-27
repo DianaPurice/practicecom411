1aa56c3e6e13e129683088663c028a44c4cbef6a
repl.it user <replituser@example.com>
2021-02-27 15:44:30 +0000

    added code to demonstrate the use of a while loop with len function.

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,13 @@
+# promt user to enter a phrase
+print("Please enter a phrase:")
+phrase = input()
+
+# declare a control variable
+bops = 0
+
+# display bops
+print()
+
+while (bops < len(phrase)):
+  print("Bop ", end ="")
+  bops = bops + 1
