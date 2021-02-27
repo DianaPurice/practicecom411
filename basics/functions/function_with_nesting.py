ac50a48914b1dbf37cd282096026507807edf0bb
repl.it user <replituser@example.com>
2021-02-27 22:26:30 +0000

    added code to demonstrate the use of a user-defined function with a parameter.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,11 @@
+# function identify
+def identify():
+  print("What lies before us?")
+  view = input()
+  if (view == "a large boulder"):
+    print("\nIt's time to run!")
+  else:
+    print("\nWe will be fine.")
+
+# call function
+identify()
